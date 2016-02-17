bc8e3ca1cd8832dfcff3c4391f43a2797398b19d
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-17 08:46:56 +0100

    Provide the executable to really execute based on the config provided in index.js.

--- bin/eslint-crewmeister.js
+++ bin/eslint-crewmeister.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
+var cli = require('../node_modules/eslint/lib/cli');
 
-console.log('i am crewmiseter');
+var path = require('path');
+var eslintConfig = path.join(__dirname, '..', 'index.js');
+
+try {
+  var argv = process.argv;
+  argv.push('--config=' + eslintConfig);
+  cli.execute(argv);
+} catch (ex) {
+  console.error(ex.message);
+  console.error(ex.stack);
+}
