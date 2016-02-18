8ef4d85fd4fcc1a90841730fd01b3dd95354bd9c
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-18 21:21:59 +0100

    Allow some more test functions.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
   globals: {
+    before: true,
+    beforeEach: true,
+    after: true,
+    afterEach: true,
     describe: true,
     it: true,
   },
