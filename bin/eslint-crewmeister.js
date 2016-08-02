46fe149e2fa90ffc8619890aa56800497128896b
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-08-02 14:36:18 +0200

    Make eslint fail on error.

--- bin/eslint-crewmeister.js
+++ bin/eslint-crewmeister.js
@@ -4,11 +4,6 @@ var cli = require('eslint/lib/cli');
 var path = require('path');
 var eslintConfig = path.join(__dirname, '..', 'index.js');
 
-try {
-  var argv = process.argv;
-  argv.push('--config=' + eslintConfig);
-  cli.execute(argv);
-} catch (ex) {
-  console.error(ex.message);
-  console.error(ex.stack);
-}
+var argv = process.argv;
+argv.push('--config=' + eslintConfig);
+cli.execute(argv);
