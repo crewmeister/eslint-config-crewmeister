d5083577f09968c7139215c20c1d95877e3fc336
Florian Forster <florian@crewmeister.com>
2017-08-15 22:59:44 +0200

    allow jsx-control-statements
    it’s nicer to read imho

--- rules/react.js
+++ rules/react.js
@@ -1,7 +1,9 @@
 module.exports = {
   'plugins': [
-    'react'
+    'react',
+    'jsx-control-statements'
   ],
+  'extends': ['plugin:jsx-control-statements/recommended'],  
   // View link below for react rules documentation
   // https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
   'rules': {
